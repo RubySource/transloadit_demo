9fbcf9ee52c7cf71dcc96b79598bcaf522a10663
Glenn Goodrich <glenn.goodrich@gmail.com>
2012-04-01 08:51:55 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Transloadit::Application.config.secret_token = '94ca66f7db3cb71a53b23f1a27c7f9a87c748bf80d492da3f2344ccddc2fb955e828c23e938968b3c25c23ba7579411389abbd720915cd3895f210a340769a73'
