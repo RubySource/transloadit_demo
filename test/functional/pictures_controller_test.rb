51b6472bdcc8033c88fcd4b3714c149f32fe8df9
Glenn Goodrich <glenn.goodrich@gmail.com>
2012-04-01 15:50:59 -0400

    Clean up tests

--- test/functional/pictures_controller_test.rb
+++ test/functional/pictures_controller_test.rb
@@ -35,24 +35,26 @@ class PicturesControllerTest < ActionController::TestCase
     end
 
     should "show name" do
-      assert_select "span.name", 1
+      assert_select "h2.name", 1
     end
 
     should "show description" do
-      assert_select "span.description", 1
+      assert_select "p.description", 1
     end
   end
 
   context "#create" do
-     
+    setup  do
+      @tl_params = "{\"assembly_id\":\"62d18d9c77594275db936099f506a4db\",\"uploads\":[{\"id\":\"d15482c0a4000710496278d21ec8172c\",\"name\":\"image001.png\",\"basename\":\"image001\",\"ext\":\"png\"}], \"template_id\": \"889870b06e184e15a13d5689a1925b61\", \"redirect_url\": \"http://localhost:3000/pictures\" }" 
+    end
     should "create a new picture" do
       assert_difference "Picture.count", 1 do
-        post :create, picture: {name: "Test Pic" , description: "This is my picture", assembly_id: "444"}
+        post :create, picture: {description: "This is my picture" }, transloadit: @tl_params
       end   
     end
 
     should "redirect to show the new picture" do
-      post :create, picture: {name: "Test Pic" , description: "This is my picture", assembly_id: "444"}
+      post :create, picture: {description: "This is my picture"}, transloadit: @tl_params
       assert_redirected_to picture_url(Picture.last)
     end
   
--- test/unit/helpers/pictures_helper_test.rb
+++ test/unit/helpers/pictures_helper_test.rb
@@ -1,4 +1,14 @@
 require 'test_helper'
 
 class PicturesHelperTest < ActionView::TestCase
+
+  context "A picture url" do
+    setup do
+      @one = pictures(:one)  
+    end
+
+    should "match the bucket and url type" do
+      assert_equal "http://s3.amazonaws.com/#{Settings.s3.bucket}/#{@one.assembly_id}/thumbnail_#{@one.name}", url_for_picture(@one, :thumbnail)
+    end
+  end
 end
